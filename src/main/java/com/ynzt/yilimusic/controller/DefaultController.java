62b73f6e25eead91ce04966a3059f6d0cce94edc
李啟林 <1401670234@qq.com>
2022-04-08 11:17:08 +0800

    '音乐项目初始化完成'

---
+++ src/main/java/com/ynzt/yilimusic/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package com.ynzt.yilimusic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+    @GetMapping("")
+    public String sayHello() {
+        return  "欢迎来到猿力音乐";
+    }
+}
